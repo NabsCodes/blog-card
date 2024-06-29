87d9c48262e69ae113deab01388caecfb55685d0
Hassan <hassanhauda@gmail.com>
2024-06-29 23:54:15 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	content: ['./index.html'],
+	theme: {
+		extend: {
+			colors: {
+				'custom-light': '#F9FAFB',
+				'custom-dark': '#D2D6DB',
+			},
+			backgroundImage: {
+				'custom-gradient': 'linear-gradient(to bottom right, #F9FAFB 0%, #D2D6DB 100%)',
+			},
+		},
+	},
+	plugins: [],
+};
